2cbdbafc61c0ce275cc5747c398322130a22388d
Anders Hesselbom <anders.hesselbom@nethouse.se>
2017-02-22 16:52:04 +0100

    Corrected button caption.

--- DemoApplication/MainWindow.Designer.cs
+++ DemoApplication/MainWindow.Designer.cs
@@ -70,7 +70,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(292, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Multiple receivers";
+            this.button4.Text = "Broadcasting to multiple receivers";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
